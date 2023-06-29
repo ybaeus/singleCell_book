bbb41118d0fcb2aee6d346d7ec05fc17f12c0dd1
Yeji Bae <dpwlek1997@gmail.com>
2023-06-29 09:03:19 -0700

    initial commit

---
+++ Chapter2.R
@@ -0,0 +1,25 @@
+"
+CHAPTER 2
+02_02_기본 코드와 개념
+"
+
+##################
+# 기본 코드 실행 #
+##################
+
+print("hi")
+
+#####################
+# R 패키지 설치하기 #
+#####################
+# R 패키지 설치
+install.packages("ggplot2")
+
+# BiocManger 패키지 설치
+install.packages("BiocManager")
+BiocManager::install("GenomicFeatures")
+
+# BiocManger 업데이트
+BiocManager::install() # BiocManager
+
+
