2b5c8d4a7313781f186cb169518b7326ea99c87f
yeji bae <47170687+ybaeus@users.noreply.github.com>
2024-05-18 23:06:47 -0700

    Update sra_download.sh

--- src/sra_download.sh
+++ src/sra_download.sh
@@ -5,6 +5,6 @@ SRR=("SRR13911909" "SRR13911910" "SRR13911911" "SRR13911912" "SRR13911913" "SRR1
 for i in "${SRR[@]}"; do
     echo "Downloading Sample: $i" 
     prefetch --max-size 100g ${i}
-    echo "The SRA file: ${i}/{i}.sra"
-    fasterq-dump ${i}/{i}.sra 
+    echo "The SRA file: ${i}/${i}.sra"
+    fasterq-dump ${i}/${i}.sra 
 done
